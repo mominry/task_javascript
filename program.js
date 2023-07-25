7ca1ec0114a25e09327f57b6a57df36889c10a54
mominry <90238146+mominry@users.noreply.github.com>
2023-07-25 11:57:27 +0530

    program.js

--- program.js
+++ program.js
@@ -51,5 +51,36 @@ function sumOfFibbonaciNumbers(num)
 }
 
 
+function findCountOfEvenOddZero(...x) {
+    let CountOfEven=0
+    let CountOfOdd=0
+    let CountOfZero=0
+
+    for (let i = 0; i < x.length; i++) {
+        
+        if (x[i]%2==0&&x[i]!=0) {
+            CountOfEven=CountOfEven+1
+        }
+        else if(x[i]%2==1){
+            CountOfOdd++
+        }
+        else
+        {
+            CountOfZero++
+        }
+
+        
+        
+        
+    }
+
+    console.log("count of odd :",CountOfOdd," count of even :",CountOfEven," count of zero :",CountOfZero);
+
+}
+
+findCountOfEvenOddZero(0,2,4,6,13,9);
+
+
+
 
 
